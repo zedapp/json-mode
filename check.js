4b68db255df0d8f4bca0159d024a031c27239b6d
Zef Hemel <zef@zef.me>
2014-03-27 14:31:12 +0100

    Promise rewrite

--- check.js
+++ check.js
@@ -3,17 +3,17 @@ var parse = require("./json_parse.js");
 /**
  * Required inputs: text
  */
-module.exports = function(info, callback) {
+module.exports = function(info) {
     var text = info.inputs.text;
     try {
         parse(text);
     } catch (e) {
         var lines = text.substring(0, e.at).split("\n");
-        return callback(null, [{
+        return [{
             row: lines.length - 1,
             text: e.message,
             type: "error"
-        }], callback);
+        }];
     }
-    callback(null, []);
+    return [];
 };
